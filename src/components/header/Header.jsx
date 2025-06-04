4616a657ed0bccc9e29e63657cb913f8e9e394e5
Mohammad Ullah <ullahmohammad314@gmail.com>
2025-06-04 23:47:46 +0600

    feat: add background blur effect to Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,10 @@ import SanchayanLogo from "./SanchayanLogo";
 
 const Header = () => {
     return (
-        <div id="header" className="sticky top-0 left-0 right-0 z-30 bg-white">
+        <div id="header" className="sticky top-0 left-0 right-0 z-30">
             <AnnouncementBar />
-            <header className="bg-white border-b border-gray-200 w-full z-50">
+            {/* add bg blur effect */}
+            <header className="bg-white/30 border-b border-gray-200 w-full z-50 shadow-md backdrop-blur-sm">
                 <div className="container mx-auto flex items-center justify-between px-4">
                     <SanchayanLogo />
                     <NavBar />
